8cf6c1d53c43662a61adb5e6fce105a711337508
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-02-22 23:10:57 +0100

    add seting XO and preventing override

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const board = (() => {
-  let board = Array(9).fill('');
+  let board = Array(9).fill(0);
   const getStatus = () => board;
   const printStatus = () => console.log(board);
-  const clear = () => board.fill('');
-  const set = (char, position) => board[position] = char;
+  const clear = () => board.fill(0);
+  const set = (value, position) => board[position] = value;
+  const getIndexValue = (index) => board[index];
 
   return {
     getStatus,
     printStatus,
     clear,
-    set
+    set,
+    getIndexValue
   };
 })();
 
@@ -31,25 +33,65 @@ const Player = (name, symbol) => {
 };
 
 const gameplayController = (() => {
-  const test = () => console.log('test');
-  const player1 = Player('Heniu', 'X');
-  const player2 = Player('Andrzy', 'O');
-  const player1Wins = () => player1.incrementScore();
-  const printScore = () => console.log(`${player1.getScore()} : ${player2.getScore()}`);
+  let _counter = 0;
+  const _player1 = Player('Heniu', 'X');
+  const _player2 = Player('Andrzy', 'O');
+  const _areas = document.querySelectorAll('.area');
 
-  const initGame = () => {
-    player1.resetScore();
-    player2.resetScore();
+  const _checkPlayer1WinningStatus = () => {
+    return (
+      parseInt(board.getStatus()[0]) + parseInt(board.getStatus()[1]) + parseInt(board.getStatus()[2]) == 3 ||
+      parseInt(board.getStatus()[3]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[5]) == 3 ||
+      parseInt(board.getStatus()[6]) + parseInt(board.getStatus()[7]) + parseInt(board.getStatus()[8]) == 3 ||
+      parseInt(board.getStatus()[0]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[8]) == 3 ||
+      parseInt(board.getStatus()[2]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[6]) == 3 
+    ) ? true : false;
+  }
+
+  const _checkPlayer2WinningStatus = () => {
+    return (
+      parseInt(board.getStatus()[0]) + parseInt(board.getStatus()[1]) + parseInt(board.getStatus()[2]) == -3 ||
+      parseInt(board.getStatus()[3]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[5]) == -3 ||
+      parseInt(board.getStatus()[6]) + parseInt(board.getStatus()[7]) + parseInt(board.getStatus()[8]) == -3 ||
+      parseInt(board.getStatus()[0]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[8]) == -3 ||
+      parseInt(board.getStatus()[2]) + parseInt(board.getStatus()[4]) + parseInt(board.getStatus()[6]) == -3
+    ) ? true : false;
+  }
+
+  const _initGame = () => {
+    _player1.resetScore();
+    _player2.resetScore();
     board.clear();
   }
 
+  const play = () => {
+    _initGame();
+    _areas.forEach(el => el.addEventListener('click', event => {
+      const index = parseInt(event.target.dataset.index);
+      console.log(index);
+      console.log(board.getIndexValue(index));
+      if (board.getIndexValue(index) == 0) {
+        if (_counter % 2 == 0) {
+          event.target.textContent = _player1.getSymbol();
+          board.set(1, index);
+          if (_checkPlayer1WinningStatus()) {
+            console.log('player 1 wins');
+          }
+        } else {
+          event.target.textContent = _player2.getSymbol();
+          board.set(-1, index);
+          if (_checkPlayer2WinningStatus()) {
+            console.log('player 2 wins');
+          }
+        }
+        _counter++;
+      }
+    }));
+  }
+
   return {
-    test,
-    initGame,
-    player1Wins,
-    printScore
+    play
   };
 })();
 
-
-
+gameplayController.play();
